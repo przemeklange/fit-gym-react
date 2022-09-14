eaa0d5a84f14feede8045b8b7a89b77a2ea5ca30
Przemysław Lange <przemek.lange@gmail.com>
2022-09-14 21:24:08 +0200

    add project and add all elements in header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Hero from './components/HeroSection/Hero'
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
