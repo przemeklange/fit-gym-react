a7a0f05dfda59d8ca1209335ca1c611859288243
Przemysław Lange <przemek.lange@gmail.com>
2022-09-15 14:38:15 +0200

    add new component on name 'Plans'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Reasons from './components/Reason/Reasons';
 import Hero from './components/HeroSection/Hero'
 import Programs from './components/Programs/Programs';
+import Plans from './components/Plans/Plans'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 }
