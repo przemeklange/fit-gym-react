c9fa42c1366dd47b833be3724b7eeed0c65c4afd
Przemysław Lange <przemek.lange@gmail.com>
2022-09-15 12:18:45 +0200

    add reaseons component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Reasons from './components/Header/Reason/Reasons';
 import Hero from './components/HeroSection/Hero'
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Reasons />
     </div>
   );
 }
