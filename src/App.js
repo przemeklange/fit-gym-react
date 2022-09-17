b5d65287bc25dad26e20f64100d3aa7670a31b41
Przemysław Lange <przemek.lange@gmail.com>
2022-09-17 20:23:38 +0200

    fix localization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Reasons from './components/Header/Reason/Reasons';
+import Reasons from './components/Reason/Reasons';
 import Hero from './components/HeroSection/Hero'
 import Programs from './components/Programs/Programs';
 
